f58fc10ade6df01a752bcd5ea35622381c58294c
ririto90 <ronengold100@gmail.com>
2025-03-06 15:48:19 -0500

    MOASC

---
+++ Logs/MOASC/2025-03-02/sub-1/004_Mar-02-2025_11:33_AM/MOASC_script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=MOASC    # Name of your job
+#SBATCH --account=multisass    # Your Slurm account
+#SBATCH --partition=debug      # Run on tier3
+#SBATCH --time=0-01:00:00       # 4 hours time limit
+#SBATCH --nodes=1              # Number of nodes
+#SBATCH --ntasks=1             # 1 task (i.e., process)
+#SBATCH --mem=16g         # Increase RAM to 32GB
+#SBATCH --gres=gpu:a100:1      # 1 a100 GPU
+#SBATCH --output=/home/rgg2706/Multimodal-Sentiment-Analysis/Logs/MOASC/2025-03-02/sub-1/004_Mar-02-2025_11:33_AM/%x_%j.out# Output file
+#SBATCH --error=/home/rgg2706/Multimodal-Sentiment-Analysis/Logs/MOASC/2025-03-02/sub-1/004_Mar-02-2025_11:33_AM/%x_%j.err  # Error file
+
+# Load necessary environment
+spack env activate default-nlp-x86_64-24072401
+
+# Set the environment variables
+export NEW_LOGS_DIR="/home/rgg2706/Multimodal-Sentiment-Analysis/Logs/MOASC/2025-03-02/sub-1/004_Mar-02-2025_11:33_AM"
+fusion=""
+dataset=""
+lr=""
+dr=""
+
+# Run the main script
+cd "/home/rgg2706/Multimodal-Sentiment-Analysis"
+
+echo "SLURM Job ID: $SLURM_JOB_ID"
+
+export PYTHONPATH=:/home/rgg2706/Multimodal-Sentiment-Analysis
+
+PYTHONPATH=$PYTHONPATH:${REPO_DIR}/Models/MOASC \
+python -u -Wd Models/MOASC/run_project.py \
+
---
+++ Logs/SIMPLE-F3/2025-02-14/sub-1/007_Feb-14-2025_02:06_PM/SIMPLE-F3_script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=SIMPLE-F3    # Name of your job
+#SBATCH --account=multisass    # Your Slurm account
+#SBATCH --partition=debug      # Run on tier3
+#SBATCH --time=0-12:00:00       # 4 hours time limit
+#SBATCH --nodes=1              # Number of nodes
+#SBATCH --ntasks=1             # 1 task (i.e., process)
+#SBATCH --mem=16g         # Increase RAM to 32GB
+#SBATCH --gres=gpu:a100:1      # 1 a100 GPU
+#SBATCH --output=/home/rgg2706/Multimodal-Sentiment-Analysis/Logs/SIMPLE-F3/2025-02-14/sub-1/007_Feb-14-2025_02:06_PM/%x_%j.out# Output file
+#SBATCH --error=/home/rgg2706/Multimodal-Sentiment-Analysis/Logs/SIMPLE-F3/2025-02-14/sub-1/007_Feb-14-2025_02:06_PM/%x_%j.err  # Error file
+
+# Load necessary environment
+spack env activate default-nlp-x86_64-24072401
+
+# Set the environment variables
+export NEW_LOGS_DIR="/home/rgg2706/Multimodal-Sentiment-Analysis/Logs/SIMPLE-F3/2025-02-14/sub-1/007_Feb-14-2025_02:06_PM"
+fusion="multiattfusion"
+dataset="mvsa-mts-v3-30"
+lr="0.001"
+dr="0.5"
+
+# Run the main script
+cd "/home/rgg2706/Multimodal-Sentiment-Analysis"
+
+echo "SLURM Job ID: $SLURM_JOB_ID"
+
+export PYTHONPATH=:/home/rgg2706/Multimodal-Sentiment-Analysis
+
+PYTHONPATH=$PYTHONPATH:${REPO_DIR}/Models/SIMPLE-F3/src/ \
+python -u -Wd Models/SIMPLE-F3/src/train.py \
+    --model_fusion "${fusion}" \
+    --dataset "${dataset}" \
+    --num_epoch 200 \
+    --batch_size 256 \
+    --log_step 16 \
+    --learning_rate "${lr}" \
+    --dropout_rate "${dr}" \
+    --weight_decay 0
